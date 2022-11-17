0ff331aad99171c8cb869a59f70f56fbe92e32fe
Benedikt Boeing <boeing@bit.uni-bonn.de>
2022-11-17 12:35:05 +0100

    delete unnecessary code

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,6 +1,4 @@
 from .neural_net import neural_net
 from .autoencoder import autoencoder
-from .deepOcc import deepOcc
-from .fcnnClassifier import fcnnClassifier
 
-__all__ = ["neural_net", "autoencoder", "deepOcc", "fcnnClassifier"]
+__all__ = ["neural_net", "autoencoder"]
